6eb4f42357f983d8fa7e3bbedda262767da5dee9
Samueel Abreu <Samueellabreu@gmail.com>
2024-06-15 01:34:29 -0300

    "Projeto quase finalizado para apresentação"

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TelaLogin from "./pages/login/TelaLogin";
+import TelaRegistro from "./pages/registro/TelaRegistro";
+import TelaHome from "./pages/home/TelaHome"; // Importe o componente da tela inicial
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<TelaLogin />} />
+        <Route path="/login" element={<TelaLogin />} />
+        <Route path="/registro" element={<TelaRegistro />} />
+        <Route path="/home" element={<TelaHome />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/registro/TelaRegistro.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./registro.css";
+import { useNavigate } from "react-router-dom";
+
+function TelaRegistro() {
+  const navigate = useNavigate();
+
+  const handleEntrarClick = () => {
+    navigate("/home");
+  }
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  }
+  return (
+    <div>
+      <div class="top-tela-registro direita-tela-registro">
+        <img src="src/img/logo.png" alt="" />
+        <h1>Venha fazer parte da maior rede social também!</h1>
+      </div>
+      <div class="bot-tela-registro esquerda-tela-registro">
+        <h1>Registrar</h1>
+        <form>
+          <fieldset>
+            <label for="name">Nome</label>
+            <input type="text" id="name" />
+            <label for="email">E-mail</label>
+            <input type="email" id="email" />
+            <label for="password">Senha</label>
+            <input type="password" id="password" />
+            <label for="password">Confirmar senha</label>
+            <input type="password" id="password" />
+
+            <div class="gender-image">
+              <div class="div-select-estiloso">
+                <label for="gender">Sexo</label>
+                <select name="gender" id="gender">
+                  <option selected disabled></option>
+                  <option value="male">Masculino</option>
+                  <option value="female">Feminino</option>
+                  <option value="others">Outros</option>
+                </select>
+              </div>
+
+              <div class="input-type-image-estiloso">
+                <label for="picture-input">Imagem</label>
+                <input type="file" accept="image/*" class="picture-input" />
+                <span class="picture-image">Selecionar imagem</span>
+              </div>
+            </div>
+
+            <button type="submit" onClick={handleEntrarClick} >Registrar-se</button>
+          </fieldset>
+        </form>
+        <a onClick={handleLoginClick}>Já possui conta?</a>
+      </div>
+    </div>
+  );
+}
+
+export default TelaRegistro;
